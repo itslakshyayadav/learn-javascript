1cd39ef1a44634a20ab4cae0a16979e553b775d2
himanshu-x <hy12632@gmail.com>
2023-06-24 15:54:15 +0530

    initial javascript commit

---
+++ tut-1.js
@@ -0,0 +1,71 @@
+console.log('lakshya world')
+let firstName;
+firstName = 'lakshya';
+console.log(firstName)
+
+let forA = 'vishal';
+console.log(forA);
+//################################################
+const pi = 3.14;
+let radius = 10;
+let circleArea = pi * radius * radius;
+console.log(circleArea)
+
+
+radius = 4;
+circleArea = pi * radius * radius;
+console.log(circleArea)
+
+//################################################
+const numbers = 20;
+let age = 20;
+let total = 20 + 20;
+console.log(total)
+
+age = 40;
+total = 20 + 40;
+console.log(total)
+
+
+//#################################################
+const multiply = 2;
+let num = 10;
+let result = 2 * 10;
+console.log(result) 
+
+num = 10;
+result = 2 * 20;
+console.log(result)
+
+//##################################################
+
+let newName;
+newName = 'Anthill';
+console.log(newName);
+
+newName = 'lakshya';
+console.log(newName); 
+
+
+age = 20;
+total = 20 + 20;
+console.log(result);
+
+//#################################################
+
+let rectangleLength = 2;
+let rectangleWidth = 2;
+rectangleArea = rectangleLength*rectangleWidth;
+console.log(rectangleArea);
+
+rectangleLength = 8;
+rectangleWidth = 9;
+rectangleArea = rectangleLength* rectangleWidth;
+console.log(rectangleArea);
+
+rectangleLength = 6;
+rectangleWidth = 9;
+rectangleArea = rectangleLength* rectangleWidth;
+console.log(rectangleArea);
+
+
---
+++ tut-2.js
@@ -0,0 +1,37 @@
+let rectangleLength = 5;
+let rectangleWidth = 2;
+rectangleArea = rectangleLength*rectangleWidth;
+console.log(rectangleArea);
+
+rectangleLength = 3;
+rectangleWidth = 4;
+rectangleArea = rectangleLength*rectangleWidth;
+console.log(rectangleArea);
+
+//################################# string concate##############################################
+
+let firstName = "lakshya";
+let lastName = "yadav";
+let age = "20";
+let fullName = firstName  +  lastName  +  age;
+console.log(fullName);
+console.log(fullName.length)
+
+let newstr = `i am ${firstName} ${lastName} and i am ${age} years old`;
+console.log(newstr);
+console.log(newstr.length)
+
+
+let key1 = "Mr.";
+let key2 = "yadav";
+let fullkey = `Hello ${key1} Lakshya ${key2} how are you .`;
+console.log(fullkey);
+console.log(fullkey.length)
+
+let name1 = "Anthill";
+let name2 = " Noida";
+let fullname = `Hello my name is ${name1}. I am live in ${name2}`;
+console.log(fullname);
+console.log(fullname.length)
+
+
