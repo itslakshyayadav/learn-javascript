3f8e488dd2bf2e57a0cbc8c6f93648b4c3623bcb
Vishal Yadav <itslakshyayadav@gmail.com>
2023-07-06 17:36:28 +0530

    document queryAll

--- data-types/object.js
+++ data-types/object.js
@@ -1,19 +1,19 @@
 "use strict;";
 
-const person = {
-  fullName: "Lakshya Yadav",
-  education: "BCA",
-  age: 21,
-  father: "Ram Avatar Yadav",
-  sports: {
-    badminton: 7,
-    tt: 9,
-  },
-  isDrinker: false,
-  favDishes: ["Aloo Gobhi", "Kathal"],
-};
+// const person = {
+//   fullName: "Lakshya Yadav",
+//   education: "BCA",
+//   age: 21,
+//   father: "Ram Avatar Yadav",
+//   sports: {
+//     badminton: 7,
+//     tt: 9,
+//   },
+//   isDrinker: false,
+//   favDishes: ["Aloo Gobhi", "Kathal"],
+// };
 
-console.log(person.favDishes[1]);
+// console.log(person.favDishes[1]);
 // console.log(person.sports.tt);
 // console.log(person["sports"]["tt"]);
 
@@ -31,11 +31,22 @@ console.log(person.favDishes[1]);
 
 // I am Lakshya Yadav, my age is 21.
 
-// person = {
-//   fullName: "Lakshya Yadav",
-//   education: "BCA",
-//   father: "Ram Avatar Yadav",
-// };
+function getColor(colorName) {
+  if (colorName) {
+    return colorName;
+  }
+  return "yellow";
+}
+
+const person = {
+  fullName: "Lakshya Yadav",
+  education: "BCA",
+  father: "Ram Avatar Yadav",
+  color: getColor,
+};
+
+console.log(person.fullName);
+console.log(person.color("red"));
 
 // console.log(
 //   `I am ${person.fullName}, my age is ${person.age}. My father's name is ${person.father}`
---
+++ document-api/queryAll.js
@@ -0,0 +1,29 @@
+function onPageLoad() {
+  console.log("page load called");
+  setDefaultResultColor();
+}
+
+function setDefaultResultColor() {
+  const resultDiv = document.getElementById("result");
+  resultDiv.style.backgroundColor = "red";
+}
+
+window.addEventListener("DOMContentLoaded", onPageLoad);
+
+function changeColor() {
+  const colorSelect = document.getElementById("colorSelect").value;
+
+  if (colorSelect) {
+    console.log(colorSelect);
+    const resultDiv = document.getElementById("result");
+    resultDiv.style.backgroundColor = colorSelect;
+    console.log(resultDiv);
+  } else {
+    alert("Please enter a valid value.");
+  }
+}
+
+function reset() {
+  document.getElementById("colorSelect").value = "red";
+  setDefaultResultColor();
+}
